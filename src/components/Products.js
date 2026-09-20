c42bb31bccfac978d22262c4a0eaf0872d30de4c
Rawan Khalifeh <rawankhalifeh2003@gmail.com>
2026-09-20 15:04:51 +0300

    first commiWkWt

--- src/components/Products.js
+++ src/components/Products.js
@@ -584,7 +584,7 @@ function Products() {
             rel="noopener noreferrer"
             className="whatsapp-link"
           >
-           تواصل معنا
+           للتواصل
           </a>
 
           {user ? (
